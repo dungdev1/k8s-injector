6dea216b13ad54933cdc61457daa9a1fb127eb7b
Dung Nguyen <dungnguyen9599@gmail.com>
2021-08-09 15:33:55 +0700

    update

--- cmd/main.go
+++ cmd/main.go
@@ -125,10 +125,12 @@ func main() {
 	}()
 
 	log.Info().Msgf("Service is ready to listen on port: %d", mainConfig.TLSPort)
-	if err := webhook.StartInjectorServer(mainConfig.TLSPort, mainConfig.CertFile, mainConfig.KeyFile); err != nil {
-		log.Fatal().Msgf("Service failed: %v", err.Error())
-	}
-	log.Info().Msgf("Started webhook server on port %s", mainConfig.TLSPort)
+	go func() {
+		if err := webhook.StartInjectorServer(mainConfig.TLSPort, mainConfig.CertFile, mainConfig.KeyFile); err != nil {
+			log.Fatal().Msgf("Service failed: %v", err.Error())
+		}
+		log.Info().Msgf("Started webhook server on port %s", mainConfig.TLSPort)
+	}()
 
 	if err := webhook.StartLifeCycleServer((mainConfig.LifecyclePort)); err != nil {
 		log.Fatal().Msgf("Service failed: %v", err.Error())
